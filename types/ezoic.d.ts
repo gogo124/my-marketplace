b951fb0ee81a85b80f6e6ea6f56df19729f018f2
gogo124 <abdelhlnx@gmail.com>
2026-09-10 01:27:27 +0100

    Harden Ezoic TypeScript typings

--- types/ezoic.d.ts
+++ types/ezoic.d.ts
@@ -1,8 +1,13 @@
+type EzoicShowAdsOptions = {
+  anchor?: string;
+  location?: string;
+};
+
 declare global {
   interface Window {
     ezstandalone?: {
       cmd: Array<() => void>;
-      showAds: (...args: any[]) => void;
+      showAds: (options?: EzoicShowAdsOptions) => void;
       destroyPlaceholders?: (...ids: number[]) => void;
       destroyAll?: () => void;
     };
