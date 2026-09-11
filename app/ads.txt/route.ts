8768387e3a06ef1e2b5a43ef9cb153630a2d8bd3
gogo124 <abdelhlnx@gmail.com>
2026-09-11 16:58:52 +0100

    Fix production ads.txt serving

--- app/ads.txt/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-const EZOIC_ADSTXT_URL = "https://srv.adstxtmanager.com/19390/moroccantrip.net";
-
-export function GET() {
-  return NextResponse.redirect(EZOIC_ADSTXT_URL, 308);
-}
